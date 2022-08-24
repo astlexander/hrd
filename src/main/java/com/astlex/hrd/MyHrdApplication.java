ba6e587a14e15f50099b5d00be925bfe1fc61ef7
Alexander Astapenko <alex.astapenko@gmail.com>
2022-08-24 23:50:21 +0300

    empty HRD (HumanResourcesDepartment) project generated by spring.io

---
+++ src/main/java/com/astlex/hrd/MyHrdApplication.java
@@ -0,0 +1,13 @@
+package com.astlex.hrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHrdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astlex/hrd/MyHrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astlex.hrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
